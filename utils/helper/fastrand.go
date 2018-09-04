0dd3130ddd992c871b08a569b3fa3e77ba5e1517
yimin <yimin@cloudwalk.cn>
2018-09-04 15:03:27 +0800

    提交

--- utils/helper/fastrand.go
+++ utils/helper/fastrand.go
@@ -5,9 +5,10 @@ import (
 	"time"
 )
 
-// Uint32 returns pseudorandom uint32.
-//
-// It is safe calling this function from concurrent goroutines.
+var(
+	rngPool sync.Pool
+)
+
 func Uint32() uint32 {
 	v := rngPool.Get()
 	if v == nil {
@@ -19,33 +20,20 @@ func Uint32() uint32 {
 	return x
 }
 
-var rngPool sync.Pool
-
-// Uint32n returns pseudorandom uint32 in the range [0..maxN).
-//
-// It is safe calling this function from concurrent goroutines.
 func Uint32n(maxN uint32) uint32 {
 	x := Uint32()
-	// See http://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
 	return uint32((uint64(x) * uint64(maxN)) >> 32)
 }
 
-// RNG is a pseudorandom number generator.
-//
-// It is unsafe to call RNG methods from concurrent goroutines.
 type RNG struct {
 	x uint32
 }
 
-// Uint32 returns pseudorandom uint32.
-//
-// It is unsafe to call this method from concurrent goroutines.
 func (r *RNG) Uint32() uint32 {
 	for r.x == 0 {
 		r.x = getRandomUint32()
 	}
 
-	// See https://en.wikipedia.org/wiki/Xorshift
 	x := r.x
 	x ^= x << 13
 	x ^= x >> 17
@@ -54,12 +42,8 @@ func (r *RNG) Uint32() uint32 {
 	return x
 }
 
-// Uint32n returns pseudorandom uint32 in the range [0..maxN).
-//
-// It is unsafe to call this method from concurrent goroutines.
 func (r *RNG) Uint32n(maxN uint32) uint32 {
 	x := r.Uint32()
-	// See http://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
 	return uint32((uint64(x) * uint64(maxN)) >> 32)
 }
 
